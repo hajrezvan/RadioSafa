7be679168af36c1f19f7df85480988bf41d13648
Haj Rezvan <rezvan1379@aut.ac.ir>
2020-09-20 10:29:55 +0430

    First commit

---
+++ Connector.java
@@ -0,0 +1,68 @@
+package com.example.radiosafa;
+
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.IOException;
+
+public class Connector implements Runnable{
+
+    private MediaPlayer mediaPlayer;
+
+    private boolean checker;
+
+    public Connector() {
+        mediaPlayer = new MediaPlayer();
+        checker = false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void run() {
+        setURL();
+        checker = initializer();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void setURL() {
+        String url = "Your URL"; // your URL here
+        mediaPlayer.setAudioAttributes(
+                new AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+        );
+        try {
+            mediaPlayer.setDataSource(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public boolean initializer() {
+        try {
+            mediaPlayer.prepare(); // might take long! (for buffering, etc)
+            return true;
+        } catch (IOException exception) {
+            System.err.println("We have a problem in play file");
+            return false;
+        }
+    }
+
+    public boolean isChecker() {
+        return checker;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void refresh() {
+        checker = initializer();
+    }
+}
